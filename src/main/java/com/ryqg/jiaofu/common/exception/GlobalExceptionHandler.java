56f19494ef8ced181ffc5dcb10073a7b50fa4ca1
czk <gyczk888@gmail.com>
2025-05-10 03:47:30 +0800

    Duplicate entry异常处理、调整exception异常返回信息

--- src/main/java/com/ryqg/jiaofu/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/ryqg/jiaofu/common/exception/GlobalExceptionHandler.java
@@ -1,11 +1,16 @@
 package com.ryqg.jiaofu.common.exception;
 
 import com.ryqg.jiaofu.common.Result;
+import com.ryqg.jiaofu.common.ResultCode;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.dao.DuplicateKeyException;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * 全局异常处理器，处理项目中抛出的业务异常
@@ -24,7 +29,7 @@ public class GlobalExceptionHandler {
             throw ex;
         }
         log.error("全局异常信息", ex);
-        return Result.failed(ex.getLocalizedMessage());
+        return Result.failed(ResultCode.SYSTEM_ERROR);
     }
 
     /**
@@ -36,5 +41,25 @@ public class GlobalExceptionHandler {
         return Result.failed(ex.getResultCode());
     }
 
+    /**
+     * 处理SQL异常
+     */
+    @ExceptionHandler(DuplicateKeyException.class)
+    public <T> Result<T> exceptionHandler(DuplicateKeyException ex){
+        String message = ex.getMessage();
+        if(message.contains("Duplicate entry")){
+            String regex = "Duplicate entry '([^']+)' for key";
+            Pattern pattern = Pattern.compile(regex);
+            Matcher matcher = pattern.matcher(message);
+            String msg =  ResultCode.USERNAME_ALREADY_EXISTS.getMsg();
+            if (matcher.find()){
+                msg = matcher.group(1) + ":" + msg;
+            }
+            return Result.failed(ResultCode.USERNAME_ALREADY_EXISTS,msg);
+        }else {
+            return Result.failed(ResultCode.SYSTEM_ERROR);
+        }
+
+    }
 
 }
