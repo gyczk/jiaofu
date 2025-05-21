fec681e01884608fe89244c89a43127ec138af4d
caozh <gyczk888@gmail.com>
2025-05-21 18:15:21 +0800

    公共批量删除

--- src/main/java/com/ryqg/jiaofu/business/common/ServiceImpl.java
+++ src/main/java/com/ryqg/jiaofu/business/common/ServiceImpl.java
@@ -1,11 +1,16 @@
 package com.ryqg.jiaofu.business.common;
 
+import cn.hutool.core.lang.Assert;
+import cn.hutool.core.util.StrUtil;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.ryqg.jiaofu.common.converter.BaseConverter;
 import com.ryqg.jiaofu.domain.dto.BaseDTO;
 import com.ryqg.jiaofu.domain.vo.BaseVO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
 
 public abstract class ServiceImpl<M extends BaseMapper<Entity>, C extends BaseConverter<Entity,DTO,VO>,Entity
         extends BaseModel,DTO extends BaseDTO,VO extends BaseVO> implements IBaseService<DTO,VO>{
@@ -39,8 +44,9 @@ public abstract class ServiceImpl<M extends BaseMapper<Entity>, C extends BaseCo
 
     @Transactional
     @Override
-    public int delete(String id) {
-        return baseMapper.deleteById(id);
+    public int delete(String ids) {
+        Assert.isTrue(StrUtil.isNotBlank(ids), "删除的用户数据为空");
+        return baseMapper.deleteBatchIds(Arrays.stream(ids.split(",")).collect(Collectors.toList()));
     }
 
 }
